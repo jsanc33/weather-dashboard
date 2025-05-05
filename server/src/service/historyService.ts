a1de77c5c28413d5d3920f0cc0cce5aacbf0d9dd
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-05-05 09:11:19 -0500

    get cities built

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
-// TODO: Define a City class with name and id properties
+//Define a City class with name and id properties
 class City {
   name: string;
   id: string;
@@ -26,8 +26,20 @@ class HistoryService {
       JSON.stringify(cities, null, '\t')
     );
   }
-  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  async getCities() {}
+  //getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+
+      try {
+        parsedCities = [].concat(JSON.parse(cities));
+      } catch (err) {
+        parsedCities = [];
+      }
+
+      return parsedCities;
+    });
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
